50a9b39a5f8402aea7d9a2bd5d95bc4f8ca9f13d
Sandeep Singh <sandeepsinghmails@gmail.com>
2018-05-02 11:22:26 +0530

    Update printSpiralMatrix.cpp

--- printSpiralMatrix.cpp
+++ printSpiralMatrix.cpp
@@ -74,8 +74,8 @@ void printSpiral(int rowEnd,
 int main()
 {
 	int a[RowMax][ColMax] = { {1, 2, 3, 4, 5, 6},
-		                      {7, 8, 9, 10, 11, 12},
-		                      {13, 14, 15, 16, 17, 18}
+		                  {7, 8, 9, 10, 11, 12},
+		                  {13, 14, 15, 16, 17, 18}
 	                        };
 
 	printSpiral(RowMax, ColMax, a);
