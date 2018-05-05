5798be36c63d98545170bb49b82b5c52746470e8
Sandeep Singh <sandeepsinghmails@gmail.com>
2018-05-05 20:12:51 +0530

    Update ReverseListInGroups.cpp

--- LinkedList/ReverseListInGroups.cpp
+++ LinkedList/ReverseListInGroups.cpp
@@ -1,1 +1,3 @@
-// Reference: https://www.geeksforgeeks.org/reverse-a-list-in-groups-of-given-size/
+// References: 
+// https://www.geeksforgeeks.org/reverse-a-list-in-groups-of-given-size/
+// https://www.geeksforgeeks.org/reverse-a-linked-list/
